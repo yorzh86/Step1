d6220cae05409e3dd8a1adf316ebe5c8f618a1da
Yorzh <yorzh.anton@Gmail.com>
2016-02-24 10:34:46 -0600

    printSimpleSystem added

--- src/main.f90
+++ src/main.f90
@@ -42,6 +42,7 @@ contains
 		call setThermostat(.false.,T0,10.0_wp*dt)
 		call setBarostat(.false.,P0, 5.0E10_wp*dt)
 		call SimpleSystem (convert(simple,'A','m'), 3, T0)
+		call printSimpleSystem()
 		!call buildSystem(convert(lattice_const,'A','m'),[2,2,2],T0)
 		!rewind(iou_lammps)
 		call doBox()
@@ -54,11 +55,11 @@ contains
 		integer::k
 		do k=0,N_steps
 			!call integrateLammps(dt)
-			if(mod(k,skip_thermo)==0) call thermoReport(k)
-			if(mod(k,skip_dump  )==0) call writeStepXYZ(iou_xyz)
-			if(mod(k,skip_neighbor)==0) call updateAllNeighbors()
-			
+			!if(mod(k,skip_thermo)==0) call thermoReport(k)
+			!if(mod(k,skip_dump  )==0) call writeStepXYZ(iou_xyz)
+			!if(mod(k,skip_neighbor)==0) call updateAllNeighbors()
 			!call velocityVerlet(dt)
+			
 			call doBox()
  			if(k==N_steps/2) call setThermostat(.false.)
 
@@ -70,6 +71,23 @@ contains
 		close(iou_thermo)
 		!close(iou_lammps)
 	end subroutine endSim
+	
+	subroutine printSimpleSystem()
+		integer::i,j
+		
+		write(*,*)'Box size[A]:',box*1E10_wp
+		write(*,*)
+		
+		write(*,'(1X, 1A5, 6A25, 1A13 )')  'id', 'rx', 'ry', 'rz', 'fx', 'fy', 'fz', 'fnorm'
+		
+		do i=1, size(atoms)
+			write(stdout, '(1X, 1I5, 6E25.15, 1F13.6)') atoms(i)%atom_id, &
+				& [(convert(atoms(i)%r(j),'m','A'),j=1,3)], &
+				& [(convert(atoms(i)%f(j), 'N', 'eV/A'), j=1,3)], &
+				& convert(fnorm(),'N','eV/A')
+		end do
+	
+	end subroutine printSimpleSystem
 
 	subroutine thermoReport(k)
 		integer,intent(in)::k
@@ -81,45 +99,20 @@ contains
 		o = heatflux()
 		
 		if (mod(c,50)==0) then
-!			write(*,*)
-!			write(stdout,'(1X,1A17,1A5, 3F6.2)')"\x1B[96mSystem size:","\x1B[97m", [(convert(box(i), 'm', 'A'), i=1,3)]
-!			write(stdout,'(1X,1A21, 1A5, 1I5)') "\x1B[96mNumber of atoms:","\x1B[97m", size(atoms)
-!			write(stdout,'(1X,1A29, 1A5, 1I3)') "\x1B[96mAv. number of neighbors:", "\x1B[97m", nint(averageNeighbors())
-!			write(*,*)			
-!			write (stdout, '(1X, 1A5, 1A4, 1A8, 1A10, 1A11, 2A18, 1A4)') "\x1B[93m", 'k[#]','temperature', &
-!				& 'tEnergy', 'Jx','Jy','Jz',  "\x1B[0m"
-		!write(*, '(1X, 1A5, 1A9, 6A13, 1A22)') 'Step', 'Temp', 'KE()', 'PE()', 'TotEng', 'Jx', 'Jy', 'Jz', 'Fnorm'
-		write(*,*)'Box size[A]:',box*1E10_wp
-		write(*,*)
-		write(*,'(1X, 1A5, 6A25, 1A13 )')  'id', 'rx', 'ry', 'rz', 'fx', 'fy', 'fz', 'fnorm'
+			write(*,*)
+			write(stdout,'(1X,1A17,1A5, 3F6.2)')"\x1B[96mSystem size:","\x1B[97m", [(convert(box(i), 'm', 'A'), i=1,3)]
+			write(stdout,'(1X,1A21, 1A5, 1I5)') "\x1B[96mNumber of atoms:","\x1B[97m", size(atoms)
+			write(stdout,'(1X,1A29, 1A5, 1I3)') "\x1B[96mAv. number of neighbors:", "\x1B[97m", nint(averageNeighbors())
+			write(*,*)			
+			write(*, '(1X, 1A5, 1A9, 6A13, 1A22)') 'Step', 'Temp', 'KE()', 'PE()', 'TotEng', 'Jx', 'Jy', 'Jz', 'Fnorm'
 		end if
-		do i=1, size(atoms)
-			write(stdout, '(1X, 1I5, 6E25.15, 1F13.6)') atoms(i)%atom_id, &
-				& [(convert(atoms(i)%r(j),'m','A'),j=1,3)], &
-				& [(convert(atoms(i)%f(j), 'N', 'eV/A'), j=1,3)], &
-				& convert(fnorm(),'N','eV/A')
-		end do
-
-!		write(stdout,'(1X,1I3,1F11.3,3F13.6,3ES17.6, 1F13.6)') k, temperature(), &
-!			& convert(KE(),'J','eV'), &
-!			& convert(PE(),'J','eV'), &
-!			& convert(E(),'J','eV'),  &
-!			& (convert(o(i),'W/m2','eV/ps/A2')/product(box),i=1,3), &
-!			& convert(fnorm(),'N','eV/A')
 		
-!		write(*,*)
-!		write(*, '(1X, 2A5, 3A15, 2A15)') 'Step', 'id', 'Fx', 'Fy', 'Fz', 'NORM2(Fatom)', 'NORM2(Fall)'
-!				!!======= CHANGE NUMBER OF N_STEPS========!!		
-!		do i=1, size(atoms)
-!			write(*,'(1X, 2I5, 3F15.9, 2F14.9 )')k, atoms(i)%atom_id, &
-!				& [(convert(atoms(i)%f(j), 'N', 'eV/A'), j=1,3)], &
-!				& convert(norm2(atoms(i)%f),'N', 'eV/A'), &
-!				& convert(fnorm(),'N','eV/A')
-!		end do
-
-
-		
-					
+		write(stdout,'(1X,1I3,1F11.3,3F13.6,3ES17.6, 1F13.6)') k, temperature(), &
+			& convert(KE(),'J','eV'), &
+			& convert(PE(),'J','eV'), &
+			& convert(E(),'J','eV'),  &
+			& (convert(o(i),'W/m2','eV/ps/A2')/product(box),i=1,3), &
+			& convert(fnorm(),'N','eV/A')
 		c = c+1
 	end subroutine thermoReport
 
